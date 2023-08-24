546d670e9a528791ec668ba45c640dc5823a7609
Aditya Khare <aditya.khr1904@gmail.com>
2023-08-24 14:50:15 +0530

    Final

--- RoomOnBoat/src/main/java/com/masai/update/UpdateName.java
+++ RoomOnBoat/src/main/java/com/masai/update/UpdateName.java
@@ -15,8 +15,8 @@ public class UpdateName {
 	@NotEmpty(message = "Name can't be Empty")
 	@NotBlank(message = "Name can't be Blank")
 	private String name;
-	@NotNull(message = "Name can't be Null")
-	@NotEmpty(message = "Name can't be Empty")
-	@NotBlank(message = "Name can't be Blank")
+	@NotNull(message = "LastName can't be Null")
+	@NotEmpty(message = "LastName can't be Empty")
+	@NotBlank(message = "LastName can't be Blank")
 	private String lastName;
 }
